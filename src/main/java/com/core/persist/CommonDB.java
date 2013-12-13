417d3d4ae6d8a08e09f28196c070ed241f0050a0
Jeong_Suk <jszzang9@expull.com>
2013-12-14 01:02:13 +0900

    mysql 연동 테스트

---
+++ src/main/java/com/core/persist/CommonDB.java
@@ -0,0 +1,8 @@
+package com.core.persist;
+
+public class CommonDB {
+	public static String MYSQL_URL = "jdbc:mysql://54.238.155.223:3306/mydb";
+	public static String MYSQL_USER = "mydb";
+	public static String MYSQL_PASSWORD = "asdf"; 
+			
+}
